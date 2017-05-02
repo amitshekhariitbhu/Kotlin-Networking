3e98ebc0d54cde66218aeaa813a64c3ca956df5b
AMIT SHEKHAR <amit.shekhar.iitbhu@gmail.com>
2017-05-02 09:47:16 +0530

    Add methods in KotError

--- kotin-networking/src/main/kotlin/com/mindorks/kotnetworking/error/KotError.kt
+++ kotin-networking/src/main/kotlin/com/mindorks/kotnetworking/error/KotError.kt
@@ -21,11 +21,43 @@ import okhttp3.Response
 /**
  * Created by amitshekhar on 01/05/17.
  */
-class KotError : Exception() {
+class KotError : Exception {
 
     var errorBody: String? = null
-    var errorCode: Int? = null
+    var errorCode = 0
     var errorDetail: String? = null
     var response: Response? = null
+
+    constructor()
+
+    constructor(response: Response) {
+        this.response = response
+    }
+
+    constructor(message: String) : super(message)
+
+    constructor(message: String, response: Response) : super(message) {
+        this.response = response
+    }
+
+    constructor(message: String, throwable: Throwable) : super(message, throwable)
+
+    constructor(message: String, response: Response, throwable: Throwable) : super(message, throwable) {
+        this.response = response
+    }
+
+    constructor(response: Response, throwable: Throwable) : super(throwable) {
+        this.response = response
+    }
+
+    constructor(throwable: Throwable) : super(throwable)
+
+    fun setCancellationMessageInError() {
+
+    }
+
+    fun <T> getErrorAsObject(objectClass: Class<T>): T? {
+        TODO("Add Logic")
+    }
 
 }
